e4ad93b004a4ff8dbd3c5144a9bb190b0e2f9e62
氢键 <42627185+JoveH-H@users.noreply.github.com>
2020-01-12 18:22:07 +0800

    Fix hh_ros v1 error

--- hh_ros/scripts/talker.py
+++ hh_ros/scripts/talker.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python
-# license removed for brevity
-import rospy
-from std_msgs.msg import String
+# coding=utf-8
+
+import rospy  # 导入rospy包 ROS的python客户端
+from std_msgs.msg import String  # 导入std_msgs.msg包的String消息类型
 
 
+# 定义发布函数 talker
 def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10)  # 10hz
+    pub = rospy.Publisher('chatter', String, queue_size=10)  # 定义发布器 pub 发布chatter
+    rospy.init_node('talker', anonymous=True)  # 初始化节点talker
+    rate = rospy.Rate(10)  # 初始化循环频率10HZ的对象rate
+
+    # 在程序没退出的情况下
     while not rospy.is_shutdown():
         hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
-        rate.sleep()
+        rospy.loginfo(hello_str)  # 消息打印到屏幕上
+        pub.publish(hello_str)  # 发布消息
+        rate.sleep()  # 休眠
 
 
 if __name__ == '__main__':
@@ -20,4 +24,3 @@ if __name__ == '__main__':
         talker()
     except rospy.ROSInterruptException:
         pass
-
