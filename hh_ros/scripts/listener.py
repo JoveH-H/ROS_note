77f35829deec0e416b7d3492b653a251d225d398
氢键 <42627185+JoveH-H@users.noreply.github.com>
2020-01-12 18:20:55 +0800

    Fix v1 version error

--- hh_ros/scripts/listener.py
+++ hh_ros/scripts/listener.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python
-from beginner_tutorials.srv import *
-import rospy
+# coding=utf-8
 
-def handle_add_two_ints(req):
-    print "Returning [%s + %s = %s]"%(req.a, req.b, (req.a + req.b))
-    return AddTwoIntsResponse(req.a + req.b) #因为我们已经将服务的类型声明为AddTwoInts，所以它会为您生成AddTwoIntsRequest对象（可以自由传递）。
+import rospy  # 导入rospy包 ROS的python客户端
+from std_msgs.msg import String  # 导入std_msgs.msg包的String消息类型
 
-def add_two_ints_server():
-    rospy.init_node('add_two_ints_server') #声明节点为add_two_ints_server
-    s = rospy.Service('add_two_ints', AddTwoInts, handle_add_two_ints)#使用AddTwoInts（之前教程中建立的）服务类型声明一个名为add_two_ints的新服务。所有请求都传递给handle_add_two_ints函数。
-    print "Ready to add two ints."
-    rospy.spin() #此语句保证直到节点被关闭，代码才会停止运行
 
-if __name__ == "__main__":
-    add_two_ints_server()
+# 回调函数
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)  # 消息打印到屏幕上
+
+
+def listener():
+    rospy.init_node('listener', anonymous=True)  # 初始化节点listener
+    rospy.Subscriber("chatter", String, callback)  # 初始化订阅器 订阅chatter
+    rospy.spin()  # 保持主进程一直循环
+
+
+if __name__ == '__main__':
+    listener()
